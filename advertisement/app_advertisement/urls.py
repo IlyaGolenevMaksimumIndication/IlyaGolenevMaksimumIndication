d3df5109847b1e5e908896fad9c43dfcee13649d
IlyaGolenevMaksimumIndication <136359143+IlyaGolenevMaksimumIndication@users.noreply.github.com>
2023-07-14 15:45:27 +0300

    Add files via upload

---
+++ advertisement/app_advertisement/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import index, top_sellers, advertisementpost, register, login, profile
+
+
+urlpatterns = [
+    path("index.html", index, name="main-page")
+    path("top-sellers.html",top-sellers,name="top_sellers")
+    path("advertisement-post.html",advertisement-post,name="advertisement-post")
+    path("register.html",register,name="register")
+    path("login.html",login,name="login")
+    path("profile.html",profile,name="profile")
+]
