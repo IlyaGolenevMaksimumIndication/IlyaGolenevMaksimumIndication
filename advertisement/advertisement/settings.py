0f420f05e4c5c52e6962f5f811a75e28ea7d3d73
IlyaGolenevMaksimumIndication <136359143+IlyaGolenevMaksimumIndication@users.noreply.github.com>
2023-07-28 17:02:12 +0300

    Add files via upload

--- advertisement/advertisement/settings.py
+++ advertisement/advertisement/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
     # our app
     'app_advertisement',
-    'app_lesson_4'
 ]
 
 MIDDLEWARE = [
@@ -79,14 +78,10 @@ WSGI_APPLICATION = 'advertisement.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': 'dbname',
-'USER': 'username',
-'PASSWORD': 'userpass',
-'HOST': '127.0.0.1',
-'PORT': '5432'
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
--- advertisement/app_advertisement/urls.py
+++ advertisement/app_advertisement/urls.py
@@ -4,11 +4,6 @@ from .views import index, top_sellers, advertisementpost, register, login, profi
 
 
 urlpatterns = [
-    path("index.html", index, name="main-page")
-    path("top-sellers.html",top-sellers,name="top_sellers")
-    path("advertisement-post.html",advertisement-post,name="advertisement-post")
-    path("register.html",register,name="register")
-    path("login.html",login,name="login")
-    path("profile.html",profile,name="profile")
-    path("advertisement",advertisement,name="advertisement")
+    path("index.html", index, name="main-page"),
+    path("top-sellers.html",top_sellers,name="top_sellers"),
 ]
